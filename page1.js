cc48e13173661d43898cbafe414a0c6658f9dc0b
DzakyNaufal <dzakynmulna@gmail.com>
2023-06-18 16:31:48 +0700

    my first commit

---
+++ page1.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("ini video FIFTY FIFTY");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini Audio FIFTY FIFTY")
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar FIFTY FIFTY");
+});
